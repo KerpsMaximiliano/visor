30f909b954cd4b9846c0e6b32b1a92db86b1dc17
i2t-Federico <federico.gauchat@i2t-sa.com.ar>
2022-08-24 14:29:51 -0300

    interfaz

--- Visor/src/app/vista-analista-funcional/vista-analista-funcional.component.ts
+++ Visor/src/app/vista-analista-funcional/vista-analista-funcional.component.ts
@@ -10,8 +10,9 @@ export class VistaAnalistaFuncionalComponent implements OnInit {
 
   actividadesNoIniciadas: Actividad[];
   actividadesEnProgreso: Actividad[];
+  actividadesEnPrueba: Actividad[];
   actividadesCompletadas: Actividad[];
-  poseeTareasEnProgreso  !: boolean;
+  // poseeTareasEnProgreso  !: boolean;
   poseeTareasCompletadas!: boolean;
 
   panelOpenState = false;
@@ -19,7 +20,7 @@ export class VistaAnalistaFuncionalComponent implements OnInit {
   constructor() {
     this.cargarActividades()
     this.actividadesNoIniciadas = [{
-      titulo: "TP 28 - xxx",
+      titulo: "NO INICIADA 1 - xxx",
       prioridad: "Alta",
       asignado: "Franco Friggeri",
       facilitador: "Adrian Enrico",
@@ -32,7 +33,7 @@ export class VistaAnalistaFuncionalComponent implements OnInit {
       notas: ""
     },
     {
-      titulo: "TP 12 - xxx",
+      titulo: "NO INICIADA 2 - xxx",
       prioridad: "Baja",
       asignado: "Luciano De Giorgio",
       facilitador: "Federico Gauchat",
@@ -44,10 +45,64 @@ export class VistaAnalistaFuncionalComponent implements OnInit {
       tareasPrecondicion: "Tarea 9",
       notas: ""
     }
-  ]
-    this.actividadesEnProgreso = [];
+    ]
+    this.actividadesEnProgreso = [{
+      titulo: "EN PROGRESO 1 - xxx",
+      prioridad: "Alta",
+      asignado: "Franco Friggeri",
+      facilitador: "Adrian Enrico",
+      fechas: "22/08/2022",
+      horasPlanificadas: 17,
+      horasEjecutadas: 15,
+      horasDesvio: 2,
+      documento: "Documento 1",
+      tareasPrecondicion: "Tarea 1",
+      notas: ""
+    },
+    {
+      titulo: "EN PROGRESO 2 - xxx",
+      prioridad: "Baja",
+      asignado: "Luciano De Giorgio",
+      facilitador: "Federico Gauchat",
+      fechas: "10/08/2022",
+      horasPlanificadas: 4,
+      horasEjecutadas: 4,
+      horasDesvio: 2,
+      documento: "Documento 2",
+      tareasPrecondicion: "Tarea 9",
+      notas: ""
+    }
+    ];
+    this.actividadesEnPrueba = [{
+      titulo: "EN PRUEBA 1 - xxx",
+      prioridad: "Alta",
+      asignado: "Franco Friggeri",
+      facilitador: "Adrian Enrico",
+      fechas: "22/08/2022",
+      horasPlanificadas: 17,
+      horasEjecutadas: 15,
+      horasDesvio: 2,
+      documento: "Documento 1",
+      tareasPrecondicion: "Tarea 1",
+      notas: ""
+    },
+    {
+      titulo: "EN PRUEBA 2 - xxx",
+      prioridad: "Baja",
+      asignado: "Luciano De Giorgio",
+      facilitador: "Federico Gauchat",
+      fechas: "10/08/2022",
+      horasPlanificadas: 4,
+      horasEjecutadas: 4,
+      horasDesvio: 2,
+      documento: "Documento 2",
+      tareasPrecondicion: "Tarea 9",
+      notas: ""
+    }
+    ]
     this.actividadesCompletadas = [{
-      titulo: "TP 28 - xxx",
+      
+      titulo: "COMPLETADA 1 - xxx",
       prioridad: "Alta",
       asignado: "Franco Friggeri",
       facilitador: "Adrian Enrico",
@@ -59,13 +114,14 @@ export class VistaAnalistaFuncionalComponent implements OnInit {
       tareasPrecondicion: "Tarea 1",
       notas: ""
     }];
-   }
+    console.log(this.actividadesCompletadas[0].titulo > "hola")
+  }
 
   ngOnInit(): void {
   }
 
   cargarActividades(){
-    this.poseeTareasEnProgreso = false;
+    //this.poseeTareasEnProgreso = false;
     this.poseeTareasCompletadas = true;
   }
 }
