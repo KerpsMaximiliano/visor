de29bd675be2bad95f991f971bb2cb142e3d64c5
LucianoDeGiorgio <luciano.degiorgio@i2t-sa.com.ar>
2022-08-25 09:02:12 -0300

    caso de uso finalizado

--- Visor/src/app/componentes/vista-disenio-tecnico/vista-disenio-tecnico.component.ts
+++ Visor/src/app/componentes/vista-disenio-tecnico/vista-disenio-tecnico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tarea } from 'src/app/interfaces/tarea';
 import { TareaService } from 'src/app/interfaces/tarea.service';
 
@@ -21,10 +21,6 @@ export class VistaDisenioTecnicoComponent implements OnInit{
   horasCompleatadas: number = 0;
   horasTotales: number = 0;
 
-  /* @ViewChild('barraRoja') divRojo?: HTMLElement;
-  @ViewChild('barraAmarilla') divAmarillo?: HTMLElement;
-  @ViewChild('barraVerde') divVerde?: HTMLElement; */
-
   constructor(private _tareaService: TareaService) {  }
 
   ngOnInit(): void {
@@ -32,21 +28,18 @@ export class VistaDisenioTecnicoComponent implements OnInit{
     this.poseeTareas();
     if (!this.noHayProyecto) {
       this.setearBarraProgreso();
+      this.ordenarListas();
     }
   }
 
-  /* ngAfterViewInit() {
-    if (!this.noHayProyecto) {
-      this.setearBarraProgreso();
-    }
-  } */
-
+  // Inyecto las tareas desde el servicio
   cargarTareas(){
     this.tareasNoIniciadas = this._tareaService.getTareasNoIniciadas();
     this.tareasEnProgreso = this._tareaService.getTareasEnProgreso();
     this.tareasCompletadas = this._tareaService.getTareasCompletadas();
   }
 
+  // Si no hay tareas, no se visualiza el proyecto
   poseeTareas() {
     if (this.tareasNoIniciadas.length != 0) {
       this.poseeTareasNoIniciadas = true;
@@ -62,6 +55,7 @@ export class VistaDisenioTecnicoComponent implements OnInit{
     }
   }
 
+  // Seteo la barra de progreso contabilizando el total de horas, y estableciendo los porcentajes y ancho de barras
   setearBarraProgreso() {
     this.tareasNoIniciadas.forEach(tarea => {
       this.horasNoIniciadas += tarea.horasPlanificadas;
@@ -91,24 +85,75 @@ export class VistaDisenioTecnicoComponent implements OnInit{
     }
   }
 
+  // Visualiza el porcentaje de tareas no iniciadas en la plantilla
   cargarBarraRoja() {
     return Math.round(this.horasNoIniciadas / this.horasTotales * 100);
   }
 
+  // Visualiza el porcentaje de tareas en progreso en la plantilla
   cargarBarraAmarilla() {
     return Math.round(this.horasEnProgreso / this.horasTotales * 100);
   }
 
+  // Visualiza el porcentaje de tareas completadas en la plantilla
   cargarBarraVerde() {
     return Math.round(this.horasCompleatadas / this.horasTotales * 100);
   }
 
+  // Calculo el desvío para retornar true si es negativo y false si es positivo
   calcularDesvio(horasPlanificadas: number, horasEjecutadas: number) {
     return (horasPlanificadas-horasEjecutadas < 0);
   }
 
+  // Retorno el valor absoluto del desvio (se debe visualizar sin signo en la plantilla)
   valorAbsoluto(numero: number) {
     return Math.abs(numero);
   }
-  
+
+  // Ordena la listas primero alfabéticamente, y luego por prioridad
+  ordenarListas() {
+    this.ordenAlfabetico(this.tareasNoIniciadas);
+    this.ordenAlfabetico(this.tareasEnProgreso);
+    this.ordenAlfabetico(this.tareasCompletadas);
+    this.tareasNoIniciadas = this.ordenPrioridad(this.tareasNoIniciadas);
+    this.tareasEnProgreso = this.ordenPrioridad(this.tareasEnProgreso);
+    this.tareasCompletadas = this.ordenPrioridad(this.tareasCompletadas);
+  }
+
+  // Ordena una lista por orden alfabético
+  ordenAlfabetico(lista: Array<Tarea>) {
+    lista.sort(function(a, b) {
+      if (a.titulo > b.titulo) {
+        return 1;
+      }
+      if (a.titulo < b.titulo) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  // Ordena una lista por prioridad
+  ordenPrioridad(lista: Array<Tarea>) {
+    let arrayOrdenado: Tarea[]=[];
+    lista.forEach(tarea => {
+    if (tarea.prioridad == 'Alta') {
+      arrayOrdenado.push(tarea);
+    }});
+    lista.forEach(tarea => {
+    if (tarea.prioridad == 'Media') {
+      arrayOrdenado.push(tarea);
+    }});
+    lista.forEach(tarea => {
+    if (tarea.prioridad == 'Baja') {
+      arrayOrdenado.push(tarea);
+    }});
+    lista.forEach(tarea => {
+    if (tarea.prioridad == '') {
+      arrayOrdenado.push(tarea);
+    }});
+    console.log(arrayOrdenado);
+    return arrayOrdenado;
+  }
+
 }
