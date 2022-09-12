f94411336a924e8c2eb5d83e2f083ea23812911a
LucianoDeGiorgio <luciano.degiorgio@i2t-sa.com.ar>
2022-09-11 23:06:04 -0300

    actualizado

--- Visor/src/app/componentes/inicio/inicio-disponibilidad-colaboradores/inicio-disponibilidad-colaboradores.component.ts
+++ Visor/src/app/componentes/inicio/inicio-disponibilidad-colaboradores/inicio-disponibilidad-colaboradores.component.ts
@@ -16,14 +16,15 @@ export class InicioDisponibilidadColaboradoresComponent implements OnInit {
   columna1!: Colaborador[];
   columna2!: Colaborador[];
   orden: string[] = ['Alfabetico', 'Tiempo Disponible'];
-  ordenSeleccion: string = 'Tiempo Disponible';
+  ordenSeleccion: string = 'Tiempo Disponible'; // se tiene que guardar en las preferencias del usuario en sesion cuando este disponible
   fechaHoy = new Date();
   fechaHastaDate = new Date();
-  minDate = new Date();
+  minDate = this.fechaHoy;
   mesesMostrados: number = 0;
   disponibilidadEquipo: number = 0;
-  capacidadTotal: number = 0;
-  planificadasTotal: number = 0;
+  tareasColaboradores: any[] = [];
+  mesesPlanificacion = { mes1: '', mes2: '', mes3: '', mes4: '', mes5: '', mes6: '', mes7: '', mes8: '', mes9: '', mes10: '', mes11: '', mes12: '' };
+  mesesPlanificacion2 = [{mes: ''}];
 
   nombre?: string;
   apellido?: string;
@@ -32,9 +33,88 @@ export class InicioDisponibilidadColaboradoresComponent implements OnInit {
   constructor(private _colaboradorService: ColaboradorService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.mesesPlanificacion.mes1 = this._colaboradorService.getMesString(this.fechaHoy);
+    this.mesesPlanificacion2[0].mes = this._colaboradorService.getMesString(this.fechaHoy);
     this.colaboradores = this._colaboradorService.getColaboradores();
-    this.actualizarHorasPlanificadas();
+    this.getTareasColaboradores();
+    this.getTareasAtrasadas();
+    this.getTiempoDisponibleColaboradores();
+    this.actualizarDisponibilidadEquipo()
     this.cambiarOrden();
+  }
+
+  getTareasColaboradores() {
+    let tareasColaboradoresAux:any = [];
+    this.colaboradores.forEach(colab => {
+      tareasColaboradoresAux.push(this._colaboradorService.getTareasColaborador(colab.id, this.fechaHoy));
+    });
+    tareasColaboradoresAux.forEach((obj: any) => {
+      obj.forEach((element: any) => {
+        this.tareasColaboradores.push(element);
+      });
+    });
+  }
+
+  getTareasAtrasadas() {
+    let contadorAtrasadas = 0;
+    this.colaboradores.forEach(colab => {
+      this.tareasColaboradores.forEach(tarea => {
+        if (tarea.idColab == colab.id && tarea.fechaPlanificacion < this.fechaHoy) {
+          if (tarea.estado == 'No iniciada' || tarea.estado == 'En progreso' || tarea.estado == 'En prueba') {
+            contadorAtrasadas++;
+          }
+        }
+      });
+      colab.atrasadas = contadorAtrasadas;
+      contadorAtrasadas = 0;
+    });
+  }
+
+  separarProyectosDelMes(id: number, mes: string) {
+    let proyectos:any = [];
+    this.tareasColaboradores.forEach(tarea => {
+      if (tarea.idColab == id && tarea.fechaPlanificacion.getMonth() == this._colaboradorService.getMesDate(mes)) {
+        if (proyectos.indexOf(tarea.proyecto) == -1) {
+          proyectos.push(tarea.proyecto);
+        }
+      }
+    });
+    return proyectos;
+  }
+
+  getPorcentajeOcupadoMensualProyecto(proyecto: string, id: number, mes: string) {
+    let hp = 0;
+    this.tareasColaboradores.forEach(tarea => {
+      if (tarea.idColab == id && tarea.proyecto == proyecto && tarea.fechaPlanificacion.getMonth() == this._colaboradorService.getMesDate(mes)) {
+        hp += tarea.horasPlanificadas
+      }
+    });
+    return Math.round((hp/this.getCapacidadColaborador(id)*100));
+  }
+
+  getCapacidadColaborador(id: number) {
+    let cap = 0;
+    this.colaboradores.forEach(colab => {
+      if (colab.id == id) {
+        cap = colab.capacidad;
+      }
+    });
+    return cap;
+  }
+
+  getPorcentajeDisponibleMensual(id: number, mes: string) {
+    let hp = 0;
+    this.tareasColaboradores.forEach(tarea => {
+      if (tarea.idColab == id && tarea.fechaPlanificacion.getMonth() == this._colaboradorService.getMesDate(mes)) {
+        hp += tarea.horasPlanificadas;
+      }
+    });
+    this.colaboradores.forEach(colab => {
+      if (colab.id == id) {
+        colab.horasPlanificadas = hp;
+      }
+    });
+    return Math.round(((this.getCapacidadColaborador(id)-hp)/this.getCapacidadColaborador(id)*100));
   }
 
   ordenarColaboradores() {
@@ -73,7 +153,7 @@ export class InicioDisponibilidadColaboradoresComponent implements OnInit {
     // agregar comportamiento para cerrar todos los expansion panel abiertos
   }
 
-  dispararCambioOrden(e: Event) {
+  dispararCambioOrdenDesdePlantilla(e: Event) {
     this.ordenSeleccion = (e.target as HTMLElement).innerText;
     this.cambiarOrden();
   }
@@ -114,6 +194,40 @@ export class InicioDisponibilidadColaboradoresComponent implements OnInit {
     }
   }
 
+  cambioFechaHasta(event: any) {
+    this.fechaHastaDate = event.value;
+    this.mesesMostrados = this.getDiferenciaMeses(this.fechaHoy, this.fechaHastaDate);
+    // aca actualizar mesesPlanificacion
+    this.getTiempoDisponibleColaboradores();
+    this.actualizarDisponibilidadEquipo();
+    this.cambiarOrden();
+  }
+
+  actualizarDisponibilidadEquipo() {
+    let horasPlanificadasAcumuladas = 0;
+    let capacidadTotalAcumulada = 0;
+    this.colaboradores.forEach(colab => {
+      horasPlanificadasAcumuladas += colab.horasPlanificadas;
+      capacidadTotalAcumulada += colab.capacidad;
+    });
+    this.disponibilidadEquipo = Math.round(this.disponibilidadEquipo = (capacidadTotalAcumulada-horasPlanificadasAcumuladas)/capacidadTotalAcumulada*100);
+  }
+
+  getTiempoDisponibleColaboradores() {
+    this.colaboradores.forEach(colab => {
+      switch (this.mesesMostrados) {
+        case 0:
+          colab.tiempoDisponible = this.getPorcentajeDisponibleMensual(colab.id, this.mesesPlanificacion.mes1);
+          break;
+        // agregar escenarios para mas meses mostrados
+      }
+    });
+  }  
+
+  getDiferenciaMeses(mesInicio: Date, mesFin: Date) {
+    return mesFin.getMonth() - mesInicio.getMonth() + (12 * (mesFin.getFullYear() - mesInicio.getFullYear()));
+  }
+
   getPorcentajeRojo(valor: number) {
     if (valor>=0 && valor<=25) {
       return true;
@@ -144,47 +258,6 @@ export class InicioDisponibilidadColaboradoresComponent implements OnInit {
     } else {
       return false;
     }
-  }
-
-  cambioFechaHasta(event: any) {
-    this.fechaHastaDate = event.value;
-    this.actualizarHorasPlanificadas();
-  }
-
-  actualizarHorasPlanificadas() {
-    this.planificadasTotal = 0;
-    this.capacidadTotal = 0;
-    this.mesesMostrados = this.getDiferenciaMeses(this.fechaHoy, this.fechaHastaDate);
-    // meter en un array cuales son los meses en string
-    // let horas: any;
-    this.colaboradores.forEach(colab => {
-      // horas = this.getHorasPlanificadasColaborador(colab.id-1, this.mesesMostrados, this.fechaHoy.getMonth()+1);
-      colab.horasPlanificadas = this.getHorasPlanificadasColaborador(colab.id-1, this.mesesMostrados, this.fechaHoy.getMonth()+1);
-      colab.tiempoDisponible = this.getTiempoDisponibleColaborador(colab);
-      colab.atrasadas = this.getTareasAtrasadas(colab);
-      this.planificadasTotal += colab.horasPlanificadas;
-      this.capacidadTotal += colab.capacidad*(this.mesesMostrados+1);
-    });
-    this.disponibilidadEquipo = Math.round(this.disponibilidadEquipo = (this.capacidadTotal-this.planificadasTotal)/this.capacidadTotal*100);
-
-    // actualizar columnas con nuevos valores por colaborador
-
-  }
-
-  getDiferenciaMeses(mesInicio: Date, mesFin: Date) {
-    return mesFin.getMonth() - mesInicio.getMonth() + (12 * (mesFin.getFullYear() - mesInicio.getFullYear()));
-  }
-
-  getHorasPlanificadasColaborador(idUser: number, diferencia: number, inicio: number) {
-    return this._colaboradorService.getHorasPlanificadas(idUser, diferencia, inicio);
-  }
-
-  getTiempoDisponibleColaborador(colaborador: Colaborador) {
-    return Math.round((colaborador.capacidad*(this.mesesMostrados+1) - colaborador.horasPlanificadas) / (colaborador.capacidad*(this.mesesMostrados+1)) * 100);
-  }
-
-  getTareasAtrasadas(colaborador: Colaborador) {
-    return this._colaboradorService.getTareasAtrasadas(colaborador.id, this.fechaHoy);
   }
 
 }
--- Visor/src/app/services/i2t/colaborador.service.ts
+++ Visor/src/app/services/i2t/colaborador.service.ts
@@ -6,11 +6,6 @@ import { Colaborador } from 'src/app/interfaces/colaborador';
 })
 export class ColaboradorService {
 
-  orden: any[] = [
-    { id: 1, nombre: 'Alfabético' },
-    { id: 2, nombre: 'Tiempo Disponible' }
-  ];
-
   colaboradores: Colaborador[] = [
     { id: 1, nombre: 'Facundo', apellido: 'Ghio Serra', capacidad: 160, horasPlanificadas: 0, tiempoDisponible: 0, atrasadas: 0 },
     { id: 2, nombre: 'Patricio', apellido: 'Macagno', capacidad: 160, horasPlanificadas: 0, tiempoDisponible: 0, atrasadas: 0 },
@@ -53,56 +48,129 @@ export class ColaboradorService {
   ];
 
   tareas: any[] = [
-    { idColab: 1, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 1, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 1, fechaPlanificacion: new Date(2022, 8, 9), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 2, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 55, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 2, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 37, completada: false, proyecto: 'Proyecto B' },
-    { idColab: 2, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 18, completada: false, proyecto: 'Proyecto C' },
-    { idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 4, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 4, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 5, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 5, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 5, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 7, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 7, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 8, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 8, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 9, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 9, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 9, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 10, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 10, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 11, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 11, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 14, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 15, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 16, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 17, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 17, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 16, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 15, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 14, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' },
-    { idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, completada: false, proyecto: 'Proyecto A' }
+    { id: 1, idColab: 1, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 2, idColab: 1, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 20, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 3, idColab: 1, fechaPlanificacion: new Date(2022, 8, 9), horasPlanificadas: 14, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 4, idColab: 2, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 55, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 5, idColab: 2, fechaPlanificacion: new Date(2022, 8, 5), horasPlanificadas: 37, estado: 'No iniciada', proyecto: 'Proyecto B' },
+    { id: 6, idColab: 2, fechaPlanificacion: new Date(2022, 8, 15), horasPlanificadas: 28, estado: 'No iniciada', proyecto: 'Proyecto C' },
+    { id: 7, idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 8, idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 9, idColab: 3, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 10, idColab: 4, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 11, idColab: 4, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 12, idColab: 5, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 13, idColab: 5, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 14, idColab: 5, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 15, idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 16, idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 17, idColab: 6, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 18, idColab: 7, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 19, idColab: 7, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 20, idColab: 8, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 21, idColab: 8, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 22, idColab: 9, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 32, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 23, idColab: 9, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto B' },
+    { id: 24, idColab: 9, fechaPlanificacion: new Date(2023, 8, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto B' },
+    { id: 25, idColab: 10, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 26, idColab: 10, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 27, idColab: 11, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 28, idColab: 11, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 29, idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 30, idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 31, idColab: 12, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 32, idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 33, idColab: 14, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 34, idColab: 15, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 35, idColab: 16, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 36, idColab: 17, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 37, idColab: 17, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 38, idColab: 16, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 39, idColab: 15, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'No iniciada', proyecto: 'Proyecto A' },
+    { id: 40, idColab: 14, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 41, idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' },
+    { id: 42, idColab: 13, fechaPlanificacion: new Date(2023, 11, 17), horasPlanificadas: 24, estado: 'En progreso', proyecto: 'Proyecto A' }
   ];
-  
-  getOrden() {
-    return this.orden;
-  }
 
   getColaboradores() {
     return this.colaboradores;
+  }
+
+  getTareasColaborador(id: number, fechaInicio: Date) {
+    let tareas:any=[];
+    this.tareas.forEach(tarea => {
+      if (tarea.idColab == id) {
+        if (tarea.fechaPlanificacion > fechaInicio) {
+          tareas.push(tarea);
+        } else if (tarea.fechaPlanificacion.getMonth() == fechaInicio.getMonth() && tarea.fechaPlanificacion.getFullYear() == fechaInicio.getFullYear()) {
+          tareas.push(tarea);
+        }
+      }
+    });
+    return tareas;
+  }
+
+  getMesString(fecha: Date) {
+    const mes = fecha.getMonth();
+    switch (mes) {
+      case 0:
+        return 'Enero';
+      case 1:
+        return 'Febrero';
+      case 2:
+        return 'Marzo';
+      case 3:
+        return 'Abril';
+      case 4:
+        return 'Mayo';
+      case 5:
+        return 'Junio';
+      case 6:
+        return 'Julio';
+      case 7:
+        return 'Agosto';
+      case 8:
+        return 'Septiembre';
+      case 9:
+        return 'Octubre';
+      case 10:
+        return 'Noviembre';
+      case 11:
+        return 'Diciembre';
+      default:
+        return '';
+    }
+  }
+
+  getMesDate(mes: string) {
+    switch (mes) {
+      case 'Enero':
+        return 0;
+      case 'Febrero':
+        return 1;
+      case 'Marzo':
+        return 2;
+      case 'Abril':
+        return 3;
+      case 'Mayo':
+        return 4;
+      case 'Junio':
+        return 5;
+      case 'Julio':
+        return 6;
+      case 'Agosto':
+        return 7;
+      case 'Septiembre':
+        return 8;
+      case 'Octubre':
+        return 9;
+      case 'Noviembre':
+        return 10;
+      case 'Diciembre':
+        return 11;
+      default:
+        return -1;
+    }
   }
 
   getHorasPlanificadas(id: number, diferenciaMes: number, inicio: number) {
@@ -133,6 +201,9 @@ export class ColaboradorService {
   }
 
   getHorasMes(id: number, mesInicio: number) {
+    if (mesInicio>12) {
+      mesInicio -= 12;
+    }
     switch (mesInicio) {
       case 1:
         return this.planificacionHoras[id].enero;
@@ -159,16 +230,6 @@ export class ColaboradorService {
       case 12:
         return this.planificacionHoras[id].diciembre;
     }
-  }
-
-  getTareasAtrasadas(id: number, fechaInicio: Date) {
-    let contadorAtrasadas = 0;
-    this.tareas.forEach(tarea => {
-      if (tarea.idColab == id && tarea.completada == false && fechaInicio > tarea.fechaPlanificacion) {
-        contadorAtrasadas += 1;
-      }
-    });
-    return contadorAtrasadas;
   }
 
 }
