09d61cdce17e151641ae756b0a00960585b7aafb
Martin <martin.greco@i2t-sa.com.ar>
2023-01-13 16:52:37 -0300

    trae el icono de linkedin

--- Visor/src/app/componentes/equipo/equipo.component.ts
+++ Visor/src/app/componentes/equipo/equipo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioRolRefact } from 'src/app/interfaces/usuarioRolRefact';
 import { UsuarioService } from 'src/app/services/i2t/usuario-rol.service';
-import { InicioEstadoProyectoComponent } from '../inicio/inicio-estado-proyecto/inicio-estado-proyecto.component';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 export interface Usuario {
   id_usuario: string,
   nombre_usuario: string,
@@ -20,6 +20,13 @@ export interface Usuario {
   carreras: Array<String>
 }
 
+// Necesario para poder importar el icono de linkedin
+const LINKEDIN_ICONO = `
+  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z" />
+</svg>
+  `;
+
 @Component({
   selector: 'app-equipo',
   templateUrl: './equipo.component.html',
@@ -31,11 +38,16 @@ export class EquipoComponent implements OnInit {
   usuariosRest: Array<UsuarioRolRefact> = [];
   usuarios: Array<Usuario> = [];
 
-  constructor(private _usuarioService: UsuarioService) { }
+  constructor(
+    private _usuarioService: UsuarioService,
+    private _iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer) {
+    _iconRegistry.addSvgIconLiteral('linkedin', sanitizer.bypassSecurityTrustHtml(LINKEDIN_ICONO));
+  }
 
   ngOnInit(): void {
     this._usuarioService.getUsuariosRefact().subscribe(respuesta => {
-      this.usuariosRest = respuesta.dataset; 
+      this.usuariosRest = respuesta.dataset;
       this.organizarUsuarios();
     });
   }
