c2fdf3ecada813b93b4e8e9b9ed0e06c7ec75997
Maximiliano <maximilianokerps@gmail.com>
2023-02-09 16:05:28 -0300

    ngx-paginations deleted.

--- Visor/src/app/interceptors/logger.interceptor.ts
+++ Visor/src/app/interceptors/logger.interceptor.ts
@@ -4,20 +4,15 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
-
 @Injectable()
 export class LoggerInterceptor implements HttpInterceptor {
-
-  constructor(
-    private router: Router
-  ) {
-  }
+  constructor(private router: Router) {}
 
   // obtenerFuncion(url: any){
   //   let func = new String(url);
@@ -33,31 +28,51 @@ export class LoggerInterceptor implements HttpInterceptor {
   //   return func;
   // }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     let basename = req.url;
     let params = JSON.parse(req.body);
 
-    console.log(`%c REST call: ${basename}`, 'background: #000; color: #fff', params);
+    console.log(
+      `%c REST call: ${basename}`,
+      'background: #000; color: #fff',
+      params
+    );
 
     return next.handle(req).pipe(
       tap((event: any) => {
-        if(event instanceof HttpResponse){
+        if (event instanceof HttpResponse) {
           let data: any = event.body;
           //original console.log(`%c REST resp: ${basename}`, 'background: #000; color: #ff0', params, data.returnset[0]);
-          if (params){
-          console.log(`%c REST resp: ${basename}`, 'background: #000; color: #ff0', params, data.returnset[0]);        
-          if (data.returnset[0].RCode == 1){
-            if (data.dataset.length == 0)
-              console.log('%c dataset vacio ', 'background: #000; color: #ff0');
-            else
-              console.log('%c dataset: ', 'background: #000; color: #ff0', data.dataset);
-          }
-          if(data.returnset[0].RCode == -6003){
-            localStorage.clear();
-            this.router.navigate(['/login']);
+          if (params) {
+            console.log(
+              `%c REST resp: ${basename}`,
+              'background: #000; color: #ff0',
+              params,
+              data.returnset[0]
+            );
+            if (data.returnset[0].RCode == 1) {
+              if (data.dataset.length == 0) {
+                console.log(
+                  '%c dataset vacio ',
+                  'background: #000; color: #ff0'
+                );
+              } else {
+                console.log(
+                  '%c dataset: ',
+                  'background: #000; color: #ff0',
+                  data.dataset
+                );
+              }
+            }
+            if (data.returnset[0].RCode == -6003) {
+              localStorage.clear();
+              this.router.navigate(['/login']);
+            }
           }
         }
-    } 
       })
     );
   }
